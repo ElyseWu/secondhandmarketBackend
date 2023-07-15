5fda30ebe5e8765a85d20b33ff54a9689dd44b44
ChanghaoLi <june.rainbowing@gmail.com>
2023-07-15 11:02:20 -0700

    add ItemImageEntity

---
+++ src/main/java/team3/secondhand/entity/ItemImageEntity.java
@@ -0,0 +1,14 @@
+package team3.secondhand.entity;
+
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("items_image")
+public record ItemImageEntity (
+        @Id
+        String url
+) {
+}
+
+
