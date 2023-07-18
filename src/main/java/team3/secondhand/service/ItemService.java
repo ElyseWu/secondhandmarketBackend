f09c0014fff2f66830500d7f4f908bf11b7dca86
wuchengyangcn <chengyangwu@utexas.edu>
2023-07-18 03:02:50 -0500

    add item service function

--- src/main/java/team3/secondhand/service/ItemService.java
+++ src/main/java/team3/secondhand/service/ItemService.java
@@ -2,6 +2,12 @@ package team3.secondhand.service;
 
 import org.springframework.stereotype.Service;
 import team3.secondhand.repository.ItemRepository;
+import team3.secondhand.entity.ItemEntity;
+import team3.secondhand.model.ItemBody;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Service
 public class ItemService {
@@ -12,5 +18,22 @@ public class ItemService {
 
     public void deleteItem(Long itemId) {
         itemRepository.deleteById(itemId);
+    }
+
+    public void modifyItem(Long itemId, ItemBody body) {
+        LocalDate localDate = LocalDate.parse(body.postedDay(), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        // update posted date?
+        // localDate = LocalDate.now();
+        Date postedDate = Date.valueOf(localDate);
+        ItemEntity item = new ItemEntity(
+                itemId,
+                body.name(),
+                body.price(),
+                body.description(),
+                body.condition(),
+                postedDate,
+                body.category(),
+                body.onSale());
+        itemRepository.save(item);
     }
 }
