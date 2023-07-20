146c967a1f4f767bcd87215c7b71ede0424dc42f
Chengyang Wu <wuchengyang.cn@gmail.com>
2023-07-20 12:48:23 -0500

    format UserRepository

--- src/main/java/team3/secondhand/repository/UserRepository.java
+++ src/main/java/team3/secondhand/repository/UserRepository.java
@@ -3,6 +3,7 @@ package team3.secondhand.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import team3.secondhand.entity.UserEntity;
+
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity,String> {
 }
