1e0521d6b6fb23767b6910f0bca7e466e2730f48
Chengyang Wu <wuchengyang.cn@gmail.com>
2023-07-20 12:42:44 -0500

    change ItemImageEntity to record

--- src/main/java/team3/secondhand/entity/ItemImageEntity.java
+++ src/main/java/team3/secondhand/entity/ItemImageEntity.java
@@ -1,16 +1,11 @@
 package team3.secondhand.entity;
 
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Table("items_image")
 public record ItemImageEntity (
-        @Id
-        String url,
-
+        @Id String url,
         Long itemId
 ) {
 }
-
-
