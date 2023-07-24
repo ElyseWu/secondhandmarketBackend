88c21613812eff010416c78c9809fc3533559e04
ChanghaoLi <cl580@duke.edu>
2023-07-23 19:08:18 -0700

    set up new branch

--- src/main/java/team3/secondhand/SecondhandApplication.java
+++ src/main/java/team3/secondhand/SecondhandApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.sql.*;
 
+// set up new search feature branch
 @SpringBootApplication
 public class SecondhandApplication {
 
