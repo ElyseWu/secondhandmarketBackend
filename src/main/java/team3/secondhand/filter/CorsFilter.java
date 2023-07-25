8047617e46b878c76ea2bcd83e9666bcf5a0919e
ChanghaoLi <cl580@duke.edu>
2023-07-24 18:41:17 -0700

    add cors feature to help front end test & change some item database filed into not null

---
+++ src/main/java/team3/secondhand/filter/CorsFilter.java
@@ -0,0 +1,31 @@
+package team3.secondhand.filter;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// 跨域资源共享，两个端口不一样，前端3000，后端8080
+// 这样不同domain发过来的请求就可以处理了
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+
+        if ("OPTIONS".equalsIgnoreCase(httpServletRequest.getMethod())) {
+            httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
+        }
+    }
+}
